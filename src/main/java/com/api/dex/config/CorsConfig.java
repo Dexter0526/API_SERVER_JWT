e78c6e7302e6803abc84ebb468f356f9b291d2e2
dexter0526 <dex9205@gmail.com>
2021-08-19 18:29:38 +0900

    cors

--- src/main/java/com/api/dex/config/CorsConfig.java
+++ src/main/java/com/api/dex/config/CorsConfig.java
@@ -32,7 +32,7 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
