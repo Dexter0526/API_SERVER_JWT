2cfa63429b3d4b918d94e8878319dece3c472acb
dexter <dex9205@gmail.com>
2021-09-06 15:53:18 +0900

    Add subscribe controller

--- src/main/java/com/api/dex/controller/SubscribeController.java
+++ src/main/java/com/api/dex/controller/SubscribeController.java
@@ -3,12 +3,17 @@ package com.api.dex.controller;
 import com.api.dex.domain.SecurityUser;
 import com.api.dex.dto.SubscribeDto;
 import com.api.dex.service.SubscribeService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -21,22 +26,42 @@ public class SubscribeController {
 
     @GetMapping("/fallow/{ownerId}")
     public ResponseEntity getSubscribePageByOwnerId(@PathVariable(value = "ownerId") Long ownerId, @RequestParam(value = "page", required = false) Integer page){
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+
         if(page == null) page = 0;
 
-        return null;
+        jsonObject.add("items", gson.toJsonTree(subscribeService.getSubscribePage(ownerId, null, page)));
+        jsonObject.addProperty("message", "success!");
+
+        return new ResponseEntity<>(gson.toJson(jsonObject), HttpStatus.OK);
     }
     @GetMapping("/like/{boardId}")
     public ResponseEntity getSubscribePageByBoardId(@PathVariable(value = "boardId") Long boardId, @RequestParam(value = "page", required = false) Integer page){
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+
         if(page == null) page = 0;
 
-        return null;
+        jsonObject.add("items", gson.toJsonTree(subscribeService.getSubscribePage(null, boardId, page)));
+        jsonObject.addProperty("message", "success!");
+
+        return new ResponseEntity<>(gson.toJson(jsonObject), HttpStatus.OK);
     }
 
     @PostMapping("/")
     public ResponseEntity insertSubscribe(@RequestBody SubscribeDto subscribeDto, Authentication authentication){
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        subscribeDto.setFallowName(securityUser.getMember().getName());
 
+        subscribeDto = subscribeService.insertSubscribe(subscribeDto);
 
-        return null;
+        jsonObject.add("items", gson.toJsonTree(subscribeDto));
+        jsonObject.addProperty("message", "success!");
+
+        return new ResponseEntity<>(gson.toJson(jsonObject), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
